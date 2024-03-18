96c8b6b21b0eae984cd5bd8f5f9f67792be21685
Santos Alarcón <santosalarcon86@gmail.com>
2024-03-18 21:10:22 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../src/components/Navbar"
+import Hero from "../src/components/Hero"
+import Highlights from "../src/components/Highlights"
+
+function App() {
+  return <main className="bg-black">
+        <Navbar />
+        <Hero />
+        <Highlights />
+    </main>
+}
+
+export default App
