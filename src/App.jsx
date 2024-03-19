c96a53d275950b00230b15a0ee2575a24897763d
Santos Alarcón <santosalarcon86@gmail.com>
2024-03-19 21:00:16 +0100

    add: 3D View Model

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Navbar from "../src/components/Navbar"
 import Hero from "../src/components/Hero"
 import Highlights from "../src/components/Highlights"
+import Model from "./components/Model"
 
 function App() {
-  return <main className="bg-black">
-        <Navbar />
-        <Hero />
-        <Highlights />
+  return (
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
     </main>
+  )
 }
 
 export default App
