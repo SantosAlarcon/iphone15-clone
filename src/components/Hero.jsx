c26c554d74f60f481767f43f14c0bae081bf6721
Santos Alarcón <santosalarcon86@gmail.com>
2024-03-19 20:26:02 +0100

    add: Video Carousel

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,11 +30,11 @@ const Hero = () => {
     <section className="w-full nav-height bg-black relative">
             <div className="h-5/6 w-full flex-center flex-col">
                 <p id="hero" className="hero-title">iPhone 15 Pro</p>
-            </div>
-            <div className="md:w-10/12 w-9/12">
-                <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
-                    <source src={videoSrc} type="video/mp4" />
-                </video>
+                <div className="md:w-10/12 w-9/12">
+                    <video className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
+                        <source src={videoSrc} type="video/mp4" />
+                    </video>
+                </div>
             </div>
 
             <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
