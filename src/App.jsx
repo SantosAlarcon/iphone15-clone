ead471f4f40257df7f19d5e2c134316c8da92208
Santos Alarcón <santosalarcon86@gmail.com>
2024-03-21 20:34:23 +0100

    Released the final version

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import Navbar from "../src/components/Navbar"
 import Hero from "../src/components/Hero"
 import Highlights from "../src/components/Highlights"
 import Model from "./components/Model"
+import * as Sentry from "@sentry/react"
+import Features from "./components/Features"
+import HowItWorks from "./components/HowItWorks"
+import Footer from "./components/Footer"
 
 function App() {
   return (
@@ -10,8 +14,11 @@ function App() {
       <Hero />
       <Highlights />
       <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
     </main>
   )
 }
 
-export default App
+export default Sentry.withProfiler(App)
